43834a741c29cdaf68db3f98403b598612bab077
klichardova <katarina.lichardova@gmail.com>
2025-04-15 18:06:26 +0200

    Update single thread test

--- examples/singleThread.cpp
+++ examples/singleThread.cpp
@@ -18,6 +18,17 @@
 #include <linux/sockios.h>
 
 #define FRAME_LENGTH 8
+const int SOCKET_TIMEOUT_US = 5*1000; // 30ms in us
+
+// --------------------------------------------------------------------------- //
+//                                EDIT HERE 
+// --------------------------------------------------------------------------- //
+
+// Id(s) and model(s) of motor(s)
+int id = 1;
+const char* can_bus = "can0";
+// --------------------------------------------------------------------------- //
+
 
 struct sockaddr_can addr;
 struct ifreq ifr;
@@ -52,20 +63,20 @@ int main()
 {
     s = openSocket();
 
-    enterMITMode(1);
+    enterMITMode(id);
 
     sleep(3);
 
-    setZeroPosition(1);
+    /*setZeroPosition(id);
 
     sleep(3);
 
     for (int i=0; i<1000; i++) {
-        stopMotor(1);
+        stopMotor(id);
         usleep(10*1000);
-    }
+    }*/
 
-    exitMITMode(1);
+    exitMITMode(id);
 
     sleep(3);
 
@@ -77,19 +88,30 @@ int main()
 
 int openSocket()
 {
-    // Open a socket to be able to communicate over a CAN network
+    // Create a CAN socket
     //int s = socket(PF_CAN, SOCK_RAW|SOCK_NONBLOCK, CAN_RAW);
     int s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
     if (s < 0) {
-        cout << "Error opening the socket! Exiting" << endl;
+        cout << "Error creating the socket! Exiting" << endl;
         exit(1);
     }
     else   
-        cout << "Socket opened successfully" << endl;
+        cout << "Socket created successfully" << endl;
 
+    // Set socket timeout
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = SOCKET_TIMEOUT_US;
+    int success = setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
+    if (success < 0) {
+        cout << "Error setting the timeout to the CAN socket" << endl;
+        exit(1);
+    }
+
+    // Bind the socket to the CAN bus
     struct sockaddr_can addr;
     struct ifreq ifr;
-    strcpy(ifr.ifr_name, "can0");
+    strcpy(ifr.ifr_name, can_bus);
     ioctl(s, SIOCGIFINDEX, &ifr);
 
 	memset(&addr, 0, sizeof(addr));  // Init
@@ -102,7 +124,7 @@ int openSocket()
         exit(1);
     }
     else
-        cout << "Binding ok " << endl;
+        cout << "Socket successfully bound to the CAN bus " << endl;
 
     return s;
 }
@@ -128,19 +150,25 @@ void enterMITMode(int id)
     nbytes = write(s, &frame, sizeof(can_frame));
     timespec endWrite = KMR::CBM::time_s();
     double written = KMR::CBM::get_delta_us(endWrite, startWrite);
-    cout << endl << "Sent MIT enter in " << written << " us" << endl;
+    cout << endl << "Sent enable command in " << written << " us" << endl;
 
     if (nbytes < 0)
-        cout << "Error entering MIT mode" << endl;
+        cout << "Error sending MIT mode request" << endl;
 
     nbytes = -1;
     // Receive frame
     timespec startConfirm = KMR::CBM::time_s();
     while(nbytes < 0) {
+        timespec startRead = KMR::CBM::time_s();
         nbytes = read(s, &frame, sizeof(can_frame));
+        timespec endRead = KMR::CBM::time_s();
+        double read = KMR::CBM::get_delta_us(endRead, startRead);
+        //cout << "Read function returned after " << read << " us" << endl;
 
         if (nbytes == 0)
             cout << "Read bytes = 0" << endl;
+        //else if (nbytes < 0)
+        //    cout << "No packet" << endl;
         else if (nbytes > 0) {
             //cout << endl;
             //cout << "CAN id: 0x" << convertToHex(frame.can_id) << endl;
@@ -154,18 +182,13 @@ void enterMITMode(int id)
             //cout << "Data 6: 0x" << convertToHex( frame.data[6]) << endl;
             //cout << "Data 7: 0x" << convertToHex( frame.data[7]) << endl;
 
-            cout << "Entered MIT mode" << endl;
+            cout << "Motor enabled" << endl;
         }
     }
 
-    struct timeval tv;
-    ioctl (s, SIOCGSTAMP, &tv);
-    timespec endConfirmCAN = KMR::CBM::convert_to_timespec(tv);
     timespec endConfirm = KMR::CBM::time_s();
     double confirm = KMR::CBM::get_delta_us(endConfirm, startConfirm);
-    double confirmCAN = KMR::CBM::get_delta_us(endConfirmCAN, startConfirm);
-    cout << "MIT entrance confirm received in " << confirm << " us" << endl;
-    cout << "CAN MIT entrance confirm received in " << confirmCAN << " us" << endl;
+    cout << "Motor enable confirmation received in " << confirm << " us" << endl;
 }
 
 
@@ -189,7 +212,7 @@ void exitMITMode(int id)
     nbytes = write(s, &frame, sizeof(can_frame));
     timespec endWrite = KMR::CBM::time_s();
     double written = KMR::CBM::get_delta_us(endWrite, startWrite);
-    cout << endl << "Sent MIT exit in " << written << " us" << endl;
+    cout << endl << "Sent disable command in " << written << " us" << endl;
 
     if (nbytes < 0)
         cout << "Error entering MIT mode" << endl;
@@ -198,7 +221,13 @@ void exitMITMode(int id)
     // Receive frame
     timespec startConfirm = KMR::CBM::time_s();
     while(nbytes < 0) {
-        nbytes = read(s, &frame, sizeof(struct can_frame));
+
+        timespec startRead = KMR::CBM::time_s();
+        nbytes = read(s, &frame, sizeof(can_frame));
+        timespec endRead = KMR::CBM::time_s();
+        double read = KMR::CBM::get_delta_us(endRead, startRead);
+        //cout << "Read function returned after " << read << " us" << endl;
+
         if (nbytes == 0)
             cout << "Read bytes = 0" << endl;
 
@@ -215,12 +244,12 @@ void exitMITMode(int id)
             //cout << "Data 6: 0x" << convertToHex( frame.data[6]) << endl;
             //cout << "Data 7: 0x" << convertToHex( frame.data[7]) << endl;
 
-            cout << "Exited MIT mode" << endl;
+            cout << "Motor disabled" << endl;
         }
     }
     timespec endConfirm = KMR::CBM::time_s();
     double confirm = KMR::CBM::get_delta_us(endConfirm, startConfirm);
-    cout << "MIT exit confirm received in " << confirm << " us" << endl;
+    cout << "Disable command confirmation received in " << confirm << " us" << endl;
 }
 
 void setZeroPosition(int id)
